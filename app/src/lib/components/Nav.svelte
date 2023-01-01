6b09ba39d8a69dc23e0073e9ddcbfccd9d6b96b1
Tyler <tylergross28@gmail.com>
2022-12-31 21:41:32 -0500

    menu support

---
+++ app/src/lib/components/Nav.svelte
@@ -0,0 +1,40 @@
+<style lang="postcss">
+  nav {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: var(--nav-height);
+    border-style: solid;
+    border-color: var(--grey);
+    border-width: 0 0 2px 0;
+    background-color: white;
+    
+    display: grid;
+    place-items: center;
+
+    @media (--tablet) {
+      top: unset;
+      bottom: 0;
+      border-width: 2px 0 0 0;
+    }
+
+    @media (prefers-color-scheme: dark) {
+      border-color: white;
+      background-color: var(--grey);
+    }
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  strong {
+    font-size: 1.25rem;
+    letter-spacing: 2px;
+  }
+</style>
+
+<nav>
+  <a href="/menu"><strong>MENU</strong></a>
+</nav>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
   import "../vars.css"
   import "../app.css"
+	import Nav from "$lib/components/Nav.svelte";
 </script>
 
 <style lang="postcss">
   main {
     margin: 0 auto;
     max-width: 660px;
+    margin-top: calc(var(--nav-height) + 1rem);
+
+    @media (--tablet) {
+      margin-top: 0;
+      margin-bottom: calc(var(--nav-height) + 1rem);
+    }
   }
 </style>
 
 <main>
   <slot />
 </main>
+<Nav />
